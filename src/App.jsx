10dbf9801be193283f1f62325c9436125dfc1277
melopez <emmanuel.m.lopez@gmail.com>
2023-06-26 17:15:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavbarComponent from "./components/NavbarComponent/NavbarComponent";
+
+
+function App() {
+
+  return (
+    <NavbarComponent />
+  )
+}
+
+export default App
---
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -0,0 +1,9 @@
+
+
+const NavbarComponent = () => {
+  return (
+    <div>NavbarComponent</div>
+  )
+}
+
+export default NavbarComponent
